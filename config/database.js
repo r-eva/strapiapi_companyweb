214b687f7ad90c0e647156b3be532419b51249a2
regina <reginaevadewi@gmail.com>
2021-03-26 21:00:18 +0100

    last changes

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '***'),
-        port: env.int('DATABASE_PORT','***'),
-        database: env('DATABASE_NAME', '***'),
-        username: env('DATABASE_USERNAME', '***'),
-        password: env('DATABASE_PASSWORD', '***'),
-        ssl: env.bool('DATABASE_SSL', '***'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi_database'),
+        username: env('DATABASE_USERNAME', 'reginaeva'),
+        password: env('DATABASE_PASSWORD', 'abc123'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
     },
